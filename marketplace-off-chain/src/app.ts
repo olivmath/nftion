7fda08e66ebeb777d02cf8610391213d417289b5
Lucas Oliveira <olivmath@protonmail.com>
2022-07-14 02:40:15 -0300

    update: server

--- marketplace-off-chain/src/app.ts
+++ marketplace-off-chain/src/app.ts
@@ -1,9 +1,17 @@
 import express from "express"
-import { openAuctions } from "./routers/auctions"
+import {
+    closedAuctions,
+    auctionsStatus,
+    openAuctions,
+    newBid
+} from "./routers/auctions"
 
 const app = express()
 
 app.use(express.json())
+app.use(auctionsStatus)
+app.use(closedAuctions)
 app.use(openAuctions)
+app.use(newBid)
 
 export { app }
--- marketplace-off-chain/src/server.ts
+++ marketplace-off-chain/src/server.ts
@@ -1,3 +1,5 @@
 import { app } from "./app"
 
-app.listen(3333)
+app.listen(3333, () => {
+    console.log(`🚀 run in http://localhost:${3333}`)
+})
