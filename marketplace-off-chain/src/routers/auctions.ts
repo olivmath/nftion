b7529e38d93b844c0497291f957304c9606f8034
Lucas Oliveira <olivmath@protonmail.com>
2022-07-14 02:39:58 -0300

    add: new route for put bids

--- marketplace-off-chain/src/routers/auctions.ts
+++ marketplace-off-chain/src/routers/auctions.ts
@@ -1,33 +1,74 @@
 import { Router } from "express"
 import { allAuctions } from "./db/ controler"
+import { Auction, Bid } from "./db/db"
 
 const router = Router()
 
-const closedAuctions = router.get("/closed", (request, response) => {
-    return response.status(200).json(allAuctions.closed.map((i) => i.nftId))
-})
-const openAuctions = router.get("/open", (request, response) => {
-    return response.status(200).json(allAuctions.open.map((i) => i.nftId))
-})
-
-const nftAuctions = router.get("/:nftId", (request, response) => {
-    const nftId = request.params.nftId
+const nftStatus = (nftId: string) => {
     const nftIsOpen = allAuctions.open.find((nft) => nft.nftId == nftId)
     const nftIsClosed = allAuctions.closed.find((nft) => nft.nftId == nftId)
 
     if (nftIsClosed == undefined && nftIsOpen == undefined) {
-        response.status(404).json({
-            message: `${nftId} NFT Auction not found`
-        })
+        return "notFound"
     } else if (nftIsClosed != undefined) {
-        response.status(202).json({
-            message: `${nftId} NFT Auction is closed`
-        })
+        return "closed"
     } else {
-        return response
-            .status(200)
-            .json(allAuctions.open.find((nft) => nft.nftId == nftId))
+        return "open"
+    }
+}
+
+const closedAuctions = router.get("/closed", (request, response) => {
+    return response.status(200).json(allAuctions.closed.map((i) => i.nftId))
+})
+
+const openAuctions = router.get("/open", (request, response) => {
+    return response.status(200).json(allAuctions.open.map((i) => i.nftId))
+})
+
+const auctionsStatus = router.get("/:nftId", (request, response) => {
+    const nftId = request.params.nftId
+    switch (nftStatus(nftId)) {
+        case "open":
+            return response
+                .status(200)
+                .json(allAuctions.open.find((nft) => nft.nftId == nftId))
+        case "closed":
+            return response.status(202).json({
+                message: `${nftId} NFT Auction is closed`
+            })
+        default:
+            return response.status(404).json({
+                message: `${nftId} NFT Auction not found`
+            })
     }
 })
 
-export { openAuctions, nftAuctions, closedAuctions }
+const newBid = router.post("/bid/:nftId", (request, response) => {
+    const nftId = request.params.nftId
+    const receivedBid: Bid = new Bid(
+        request.body.signature,
+        request.body.addr,
+        request.body.bid
+    )
+    if (nftStatus(nftId) == "open") {
+        const nftAuction: Auction = allAuctions.open.filter(
+            (auction) => auction.nftId == nftId
+        )[0]
+        if (nftAuction.initPrice > receivedBid.bid) {
+            return response.status(404).json({
+                message: `${receivedBid.bid} is less than that initial price: ${nftAuction.initPrice}`
+            })
+        } else {
+            nftAuction.addNewBid(receivedBid)
+            return response.status(201).json({
+                yourBid: nftAuction.bids.findIndex(bid => bid.addr == receivedBid.addr)
+            })
+        }
+    } else {
+        return response.status(404).json({
+            message: `${nftId} NFT Auction is not available`
+        })
+    }
+})
+
+export { openAuctions, closedAuctions, auctionsStatus, newBid }
