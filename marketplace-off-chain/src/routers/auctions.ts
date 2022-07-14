8612b7a6a9e499c47a783df8b14c327202099125
Lucas Oliveira <olivmath@protonmail.com>
2022-07-14 01:00:43 -0300

    update: linter

--- marketplace-off-chain/src/routers/auctions.ts
+++ marketplace-off-chain/src/routers/auctions.ts
@@ -4,10 +4,10 @@ import { allAuctions } from "./db/ controler"
 const router = Router()
 
 const closedAuctions = router.get("/closed", (request, response) => {
-    return response.status(200).json(allAuctions.closed.map(i => i.nftId))
+    return response.status(200).json(allAuctions.closed.map((i) => i.nftId))
 })
 const openAuctions = router.get("/open", (request, response) => {
-    return response.status(200).json(allAuctions.open.map(i => i.nftId))
+    return response.status(200).json(allAuctions.open.map((i) => i.nftId))
 })
 
 const nftAuctions = router.get("/:nftId", (request, response) => {
@@ -15,16 +15,18 @@ const nftAuctions = router.get("/:nftId", (request, response) => {
     const nftIsOpen = allAuctions.open.find((nft) => nft.nftId == nftId)
     const nftIsClosed = allAuctions.closed.find((nft) => nft.nftId == nftId)
 
-    if(nftIsClosed == undefined && nftIsOpen == undefined) {
+    if (nftIsClosed == undefined && nftIsOpen == undefined) {
         response.status(404).json({
-            "message": `${nftId} NFT Auction not found`,
+            message: `${nftId} NFT Auction not found`
         })
-    } else if(nftIsClosed != undefined) {
+    } else if (nftIsClosed != undefined) {
         response.status(202).json({
-            "message": `${nftId} NFT Auction is closed`,
+            message: `${nftId} NFT Auction is closed`
         })
     } else {
-        return response.status(200).json(allAuctions.open.find(nft => nft.nftId == nftId))
+        return response
+            .status(200)
+            .json(allAuctions.open.find((nft) => nft.nftId == nftId))
     }
 })
 
