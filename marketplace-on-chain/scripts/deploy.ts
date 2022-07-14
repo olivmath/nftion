5a78483990b5ec6a35556c5bf19998d802ae20d3
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2022-07-13 22:49:59 -0300

    add: erc-20 token `lua` (#5)
    * add: erc-20 token `lua`
    
    * update: pipeline
    
    * update: pipeline
    
    * update: package.json
    
    * update: package.json
    
    * add: erc20 token
    
    * update: prettier
    
    * update: pipeline
    
    * update: pipeline
    
    * update: pipeline

--- marketplace-on-chain/scripts/deploy.ts
+++ marketplace-on-chain/scripts/deploy.ts
@@ -1,23 +1,44 @@
-import { ethers } from "hardhat";
+// import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+//   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+//   const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+//   const Lock = await ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+//   await lock.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+//   console.log("Lock with 1 ETH deployed to:", lock.address);
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+const hre = require("hardhat")
+
+async function deploy() {
+    console.log("🕐 Deploying smart-contract...")
+    const [owner, addr1] = await hre.ethers.getSigners()
+    const initBalance = await owner.getBalance()
+
+    const contractFactory = await hre.ethers.getContractFactory("Lua")
+    const deployedContract = await contractFactory.deploy(100)
+
+    const endBalance = await owner.getBalance()
+    console.log(`✅ Contract Deployed Address: ${deployedContract.address}`)
+    console.log(`🔑 Contract Owner: ${owner.address}`)
+    console.log(`💸 Deploy spend: ${(initBalance - endBalance) / 1e18} ethers`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
