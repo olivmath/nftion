2428bfa11bdecda4217b9166346814f946050a1d
Lucas Oliveira <olivmath@protonmail.com>
2022-07-15 01:49:57 -0300

    update: linter

--- marketplace-off-chain/src/routers/seller.ts
+++ marketplace-off-chain/src/routers/seller.ts
@@ -36,29 +36,46 @@ const newAuction = router.post("/auction", (request, response) => {
     return response.status(201).json(newAuction)
 })
 
-const closedAuction = router.delete("/auction/:nftId", async (request, response) => {
-    const [signature, seller, nftId] = [request.body.signature, request.body.seller, request.params.nftId]
-    if (signature == undefined || seller == undefined || nftId == undefined) {
-        return response.status(404).json({
-            message: "not found params: `signature`, `seller`"
-        })
-    } else {
-        if(nftStatus(nftId) == "open") {
-            if(!ethers.utils.verifyMessage(nftId, signature) == seller) {
-                throw new Error("Signature not valid")
+const closedAuction = router.delete(
+    "/auction/:nftId",
+    async (request, response) => {
+        const [signature, seller, nftId] = [
+            request.body.signature,
+            request.body.seller,
+            request.params.nftId
+        ]
+        if (
+            signature == undefined ||
+            seller == undefined ||
+            nftId == undefined
+        ) {
+            return response.status(404).json({
+                message: "not found params: `signature`, `seller`"
+            })
+        } else {
+            if (nftStatus(nftId) == "open") {
+                if (!ethers.utils.verifyMessage(nftId, signature) == seller) {
+                    throw new Error("Signature not valid")
+                } else {
+                    allAuctions.closed.push(
+                        allAuctions.open.filter(
+                            (auction) => auction.nftId == nftId
+                        )[0]
+                    )
+                    const index = allAuctions.open.findIndex(
+                        (auction) => auction.nftId == nftId
+                    )
+                    allAuctions.open.splice(index, 1)
+                    return response.status(201).json({
+                        message: `${nftId} closed`
+                    })
+                }
             } else {
-                allAuctions.closed.push(allAuctions.open.filter(auction => auction.nftId == nftId)[0])
-                const index = allAuctions.open.findIndex(auction => auction.nftId == nftId)
-                allAuctions.open.splice(index, 1)
-                return response.status(201).json({
-                    message: `${nftId} closed`
+                return response.status(404).json({
+                    message: `${nftId} is already closed or not found`
                 })
             }
-        } else {
-            return response.status(404).json({
-                message: `${nftId} is already closed or not found`
-            })
         }
     }
-})
+)
 export { newAuction, closedAuction }
