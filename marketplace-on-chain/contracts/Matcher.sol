6dcc0825002d7919a6f0352a932605f49002a840
Lucas Oliveira <olivmath@protonmail.com>
2022-07-27 22:02:50 -0300

    add: validation signature into matcher

--- marketplace-on-chain/contracts/Matcher.sol
+++ marketplace-on-chain/contracts/Matcher.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract Matcher {}
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+
+contract Matcher {
+    using SignatureChecker for address;
+
+    function settle(bytes32 hash, bytes memory signature) public view {
+        address signer = msg.sender;
+
+        signer.isValidSignatureNow(hash, signature);
+    }
+}
