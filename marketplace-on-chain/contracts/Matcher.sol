33dc3a035e9fd9c67dd272b96f02d0f102a05903
Lucas Oliveira <olivmath@protonmail.com>
2022-07-28 00:57:59 -0300

    update: matcher contract with structs

--- marketplace-on-chain/contracts/Matcher.sol
+++ marketplace-on-chain/contracts/Matcher.sol
@@ -6,6 +6,20 @@ import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 contract Matcher {
     using SignatureChecker for address;
 
+    struct Seller {
+        address ERC721Contract;
+        bytes32 message;
+        bytes signature;
+        uint256 NFTid;
+    }
+
+    struct Bidder {
+        address ERC20Contract;
+        bytes32 message;
+        bytes signature;
+        uint256 bid;
+    }
+
     function settle(bytes32 message, bytes memory signature)
         public
         view
@@ -16,5 +30,22 @@ contract Matcher {
         } else {
             return false;
         }
+
+        // validate seller signature
+        // validate bidder signature
+        // keccak256(bid) == bidder.message
+        // keccak256(NFTid) == seller.message
+
+        // SWAP
+        // bidder.ERC20Contract.call{gas: 50000}(
+        //     abi.encodeWithSignature(
+        //         transferFrom(bidder.addr, seller.addr, bidder.bid)
+        //     )
+        // );
+        // seller.ERC721Contract.call{gas: 50000}(
+        //     abi.encodeWithSignature(
+        //         transferFrom(seller.addr, bidder.addr, seller.NFTid)
+        //     )
+        // );
     }
 }
