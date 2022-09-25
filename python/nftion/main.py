bcf42600c6c90125b1e1d43b9198efda9b663c7e
Lucas Oliveira <olivmath@protonmail.com>
2022-09-25 14:29:18 -0300

    add: auction model

--- python/nftion/main.py
+++ python/nftion/main.py
@@ -1,6 +1,10 @@
 # pylint: disable=no-name-in-module
 
+from nftion.api.auction import closed_auctions
 from nftion.api.auction import open_auctions
+from nftion.api.auction import all_auctions
 from nftion import app
 
 app.include_router(open_auctions.router)
+app.include_router(closed_auctions.router)
+app.include_router(all_auctions.router)
--- python/nftion/models/auction.py
+++ python/nftion/models/auction.py
@@ -2,9 +2,39 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=no-self-argument
 
-from nftion.models.bid import Bid
-from pydantic import BaseModel
 from typing import List
+from pydantic import BaseModel
+
+
+class Seller(BaseModel):
+    """
+    # Mock NFT Seller
+    """
+    prvk: str = "0x7f27c4189fb97fd29fba63779c2bcfc8bbb066c403d2754abc52283ca502cc4f"
+    addr: str = "0xFe32CA2F5278514AAeB6B46Dadb0282f1B916EbA"
+    initial_price: int = 10
+
+
+class Bid(BaseModel):
+    """
+    # Bid
+    """
+    bid: int
+    bidder: str
+    signature: str
+
+    # @validator("*")
+    # def validate_signature(cls, v) -> bool:
+    #     return v
+    # @validator("*")
+    # def validate_approved_by_contract(cls, v) -> bool:
+    #     return v
+    # @validator("*")
+    # def validate_balance_of_bidder(cls, v) -> bool:
+    #     return v
+    # @validator("*")
+    # def validate_allowance_of_bidder(cls, v) -> bool:
+    #     return v
 
 
 class Auction(BaseModel):
@@ -12,9 +42,21 @@ class Auction(BaseModel):
     # Auction of a NFT
     """
     init_price: int
-    bids: List[Bid]
-    nft_id: str
     seller: str
+    nft_id: str
+    signature: str
+    bids: List[Bid]
+
+    def __init__(
+        self,
+        **kwargs
+    ) -> None:
+        kwargs['bids'] = [Bid(
+            bid=kwargs['init_price'],
+            bidder=kwargs['seller'],
+            signature=kwargs['signature']
+        )]
+        super().__init__(**kwargs)
 
     def add_new_bid(cls, new_bid: Bid):
         if cls.bids[0].bid >= new_bid.bid:
@@ -22,3 +64,11 @@ class Auction(BaseModel):
                 f"{new_bid.bid} is insufficient, bid more than {cls.bids[0].bid}"
             )
         cls.bids.insert(0, new_bid)
+
+
+class AuctionsOpenClosed(BaseModel):
+    """
+    # All Auction open and closed
+    """
+    open_auctions: List[Auction]
+    closed_auctions: List[Auction]
