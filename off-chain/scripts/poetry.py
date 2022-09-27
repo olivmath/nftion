eb202a93b136ccba516bbb5cb46dd5018b39baa6
Lucas Oliveira <olivmath@protonmail.com>
2022-09-26 22:44:13 -0300

    add: ci pipeline

--- off-chain/scripts/poetry.py
+++ off-chain/scripts/poetry.py
@@ -1,5 +1,22 @@
 import subprocess
 
-# running `poetry run up` for up server
+
 def up():
+    """
+    # running `poetry run up` for up server
+    """
     subprocess.run(["uvicorn", "nftion.main:app", "--reload"])
+
+
+def lint():
+    """
+    # running `poetry run lint` for lint
+    """
+    subprocess.run(["pylint", "nftion"])
+
+
+def b():
+    """
+    # running `poetry run t` for behave test 
+    """
+    subprocess.run(["behave", "tests/behave/features"])
